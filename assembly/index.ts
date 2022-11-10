386152141cb60e2c249d4a6b24f196d4633071ce
Andrew Starling <tobogganhill@gmail.com>
2022-11-10 03:30:34 +0000

    created Greeting contract

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,13 +1,13 @@
 import {storage, Context} from "near-sdk-as";
 
-class Greeting {
+export class Greeting {
 
     getGreeting(accountId: string): string|null {
 
         return storage.get<string>(accountId,"Greeting not available");
     }
 
-    setGreeting(greeting: string) {
+    setGreeting(greeting: string): void {
         storage.set(Context.sender, greeting);
     }
 }
