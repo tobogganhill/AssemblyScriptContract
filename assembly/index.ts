a7c7132e9d85b09c7f88aea0775af06e9a204b8f
Andrew Starling <tobogganhill@gmail.com>
2022-11-11 02:13:59 +0000

    Hello World contract

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,13 +1,15 @@
-import {storage, Context} from "near-sdk-as";
+import {storage, Context, logging} from "near-sdk-as";
 
 export class Greeting {
 
     getGreeting(accountId: string): string|null {
-
+        logging.log("Retrieving greeting for " + accountId);
         return storage.get<string>(accountId,"Greeting not available");
     }
 
     setGreeting(greeting: string): void {
+        logging.log("Setting greeting for " + Context.sender);
+        logging.log("Setting greeting to " + greeting);
         storage.set(Context.sender, greeting);
     }
 }
